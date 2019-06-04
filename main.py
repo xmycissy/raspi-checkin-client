bab072c293ef8ed1b5b5bf4a0e1ebfa6aace415a
kotoyuuko <kokoro@core.moe>
2019-06-04 21:41:57 +0800

    修复元组赋值问题

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def userLogout(userID):
         if log['id'] != userID:
             continue
 
-        log['logs'][-1][1] = time.time()
+        log['logs'][-1] = (log['logs'][0], time.time())
         print('user %s logout' % userID)
         return
 
